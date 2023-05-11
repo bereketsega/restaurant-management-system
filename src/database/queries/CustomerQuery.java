9bcf24d31d85616f1b75c475a4d140e0247fbdde
Pingu <64817522+Arctic-Bird@users.noreply.github.com>
2023-05-11 16:58:57 -0400

    Update CustomerQuery.java

--- src/database/queries/CustomerQuery.java
+++ src/database/queries/CustomerQuery.java
@@ -1,7 +1,7 @@
 package database.queries;
 
 public class CustomerQuery {
-    public static String createCustomer(int customerID, String customerFName, String customerLName, String customerSSN, long customerPhone, String customerAddress) {
+    public static String createCustomer(int customerID, String customerFName, String customerLName, String customerSSN, String customerPhone, String customerAddress) {
         return "INSERT INTO CUSTOMER (customerID, customerFName, customerLName, customerSSN, customerPhone, customerAddress) " +
                 "VALUES (" + customerID + ", '" + customerFName + "', '" + customerLName + "', " + customerSSN + ", " + customerPhone + ", '" + customerAddress + "')";
     }
