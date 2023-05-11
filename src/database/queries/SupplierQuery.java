9f9490d96c76c6bc5ce80d419f8da56b697cf823
Pingu <64817522+Arctic-Bird@users.noreply.github.com>
2023-05-11 16:59:12 -0400

    Update SupplierQuery.java

--- src/database/queries/SupplierQuery.java
+++ src/database/queries/SupplierQuery.java
@@ -1,7 +1,7 @@
 package database.queries;
 
 public class SupplierQuery {
-    public static String createSupplier(String supplierID, long supplierPhone, String supplierCity, String supplierAddress) {
+    public static String createSupplier(String supplierID, String supplierPhone, String supplierCity, String supplierAddress) {
         return "INSERT INTO SUPPLIER (supplierID, supplierPhone, supplierCity, supplierAddress) " +
                 "VALUES (" + supplierID + ", " + supplierPhone + ", '" + supplierCity + "', '" + supplierAddress + "')";
     }
